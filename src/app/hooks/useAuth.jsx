83f1cca2c158c24c37e4f3ed4f9fd2ebfc4c33c7
Vladislav Urykov <vladislavurykov29@gmail.com>
2023-03-04 05:32:15 +0300

    protected route

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -20,7 +20,7 @@ export const useAuth = () => {
 };
 
 const AuthProvider = ({ children }) => {
-    const [currentUser, setUser] = useState({});
+    const [currentUser, setUser] = useState();
     const [error, setError] = useState(null);
 
     function getRandomNun(min, max) {
